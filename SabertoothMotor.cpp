835efb4f86259e7d7726db6745205587b7ff0a98
au79755 <55516656+au79755@users.noreply.github.com>
2019-09-24 00:58:02 -0500

    Update SabertoothMotor.cpp

--- SabertoothMotor.cpp
+++ SabertoothMotor.cpp
@@ -1,27 +1,30 @@
 #include "SabertoothMotor.h"
 
-SabertoothMotor::SabertoothMotor(){}
+SabertoothMotor::SabertoothMotor() {}
 
 void SabertoothMotor::init (HardwareSerial stream, int addr, int max_vel) {
-	STREAM = &stream;
-	ADDR = addr;
-	MAX_VEL = max_vel;
+  STREAM = &stream;
+  ADDR = addr;
+  MAX_VEL = max_vel;
 }
 
 void SabertoothMotor::run (int cmd, int data) {
-	HardwareSerial temp = *STREAM;
-	temp.write (ADDR);
-	temp.write (cmd);
-	temp.write (data);
-	temp.write ((ADDR + cmd + data) & 127);
+  (*STREAM).write (ADDR);
+  (*STREAM).write (cmd);
+  (*STREAM).write (data);
+  (*STREAM).write ((ADDR + cmd + data) & 127);
 }
 
 void SabertoothMotor::run (int cmd, double r) {
   run (cmd, MAX_VEL * r);
 }
 
-void SabertoothMotor::fwd(int data, int n) { run (n == 0 ? FWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, data); }
-void SabertoothMotor::bwd(int data, int n) { run (n == 0 ? BWD_MIX : n == 1 ? MOTOR1_BWD : MOTOR2_BWD, data); }
+void SabertoothMotor::fwd(int data, int n) {
+  run (n == 0 ? FWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, data);
+}
+void SabertoothMotor::bwd(int data, int n) {
+  run (n == 0 ? BWD_MIX : n == 1 ? MOTOR1_BWD : MOTOR2_BWD, data);
+}
 void SabertoothMotor::lht(int data, bool mix) {
   if (mix) { //mixed motor control
     run (LHT_MIX, data);
@@ -36,11 +39,15 @@ void SabertoothMotor::rht(int data, bool mix) {
   } else { //separate motor control
     run (MOTOR1_BWD, data);
     run (MOTOR2_FWD, data);
-  }  
+  }
 }
 
-void SabertoothMotor::fwd(double r, int n) { run (n == 0 ? FWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, r); }
-void SabertoothMotor::bwd(double r, int n) { run (n == 0 ? BWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, r); }
+void SabertoothMotor::fwd(double r, int n) {
+  run (n == 0 ? FWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, r);
+}
+void SabertoothMotor::bwd(double r, int n) {
+  run (n == 0 ? BWD_MIX : n == 1 ? MOTOR1_FWD : MOTOR2_FWD, r);
+}
 void SabertoothMotor::lht(double r, bool mix) {
   if (mix) { //mixed motor control
     run (LHT_MIX, r);
