9e31a9d070fa63a17d52a70bbe8b157c99d6f7f1
au79755 <55516656+au79755@users.noreply.github.com>
2019-10-30 20:35:28 -0500

    Add files via upload

--- Sabertooth/SabertoothMotor.cpp
+++ Sabertooth/SabertoothMotor.cpp
@@ -2,167 +2,77 @@
 
 SabertoothMotor::SabertoothMotor() {}  // Sabertooth motor object init
 
-void SabertoothMotor::init (int serial_idx, int addr, int max_vel) {  
-  SERIAL_IDX = serial_idx;
-  ADDR = addr;
-  MAX_VEL = max_vel;
+void SabertoothMotor::init (int addr [5]) { //addresses of the 2x60 Sabertooth boards connected to Serial
+	for (int i = 0; i < 5; i++) {
+    ADDR [i] = addr [i];
+	}
 }
 
-/* *************************************************************************** */
-
-/*
-run commands that send the commands to the sabertooth? One for the int fn's and one for double fn's? Maybe? I guess..?
-*/
-
-void SabertoothMotor::run (int cmd, int data) {  // Int fn's
-  data = min(MAX_VEL, data);
-  switch (SERIAL_IDX) {
-    case 1: 
-	Serial1.write(ADDR); 
+void SabertoothMotor::run (int addr, int cmd, int data) {
+	data = data;
+	Serial1.write(addr); 
 	Serial1.write(cmd); 
 	Serial1.write(data); 
-	Serial1.write((ADDR + cmd + data) & 127); 
-	break;
-    case 2: 
-	Serial2.write(ADDR); 
-	Serial2.write(cmd); 
-	Serial2.write(data); 
-	Serial2.write((ADDR + cmd + data) & 127); 
-	break;
-    case 3: 
-	Serial3.write(ADDR); 
-	Serial3.write(cmd); 
-	Serial3.write(data); 
-	Serial3.write((ADDR + cmd + data) & 127); 
-	break;
-    default: break; 
+	Serial1.write((addr + cmd + data) & 127);
+}
+
+void SabertoothMotor::fwd(int addr, int data, int n) {  // n = 0 by default
+	switch (n) {
+		case 0:  // Both motors -> technically never used
+		run (addr, MOTOR1_FWD, data);
+		run (addr, MOTOR2_FWD, data);
+		break;
+	case 1:  // First motor
+		run (addr, MOTOR1_FWD, data);
+		break;
+	case 2:  // Second motor
+		run (addr, MOTOR2_FWD, data);
+   		break;
+	default: break;
+  }
+}
+void SabertoothMotor::bwd(int addr, int data, int n) {
+	switch (n) {
+		case 0: // Both motors
+		run (addr, MOTOR1_BWD, data);
+		run (addr, MOTOR2_BWD, data);
+		break;
+  	  case 1: // First motor
+		run (addr, MOTOR1_BWD, data);
+		break;
+	case 2:  // Second motor
+		run (addr, MOTOR2_BWD, data);
+		break;
+	default: break;
   }
 }
 
-void SabertoothMotor::run (int cmd, double r) {  // Double fn's
-  run (cmd, (int)(MAX_VEL * r));
+void SabertoothMotor::halt (int addr) {
+  run (addr, 0, 0);  // Stops motor one
+  run (addr, 4, 0);  // Stops motor two
 }
 
-/* *************************************************************************** */
-
-/*
-Runs the robot using speed variables as ints
-EX: fwd(200, 0)
-    -Runs both motors at 200 whatever's
-    -Only runs when r sent is an int
-*/
-
-void SabertoothMotor::fwd(int data, int n) {  // n = 0 by default
-  switch (n) {
-    case 0:  // Both motors
-      run (MOTOR1_FWD, data);
-      run (MOTOR2_FWD, data);
-      break;
-    case 1:  // First motor
-      run (MOTOR1_FWD, data);
-      break;
-    case 2:  // Second motor
-      run (MOTOR2_FWD, data);
-      break;
-    default: break;
+void SabertoothMotor::read (String msg) { //input a string msg, presumably from the odroid, execute it
+  //format: #[motor]$[power]
+  int a = msg.indexOf('#') + 1,
+    b = msg.indexOf('$'),
+    c = msg.length ();
+  String p1 = msg.substring (a, b),
+    p2 = msg.substring (b + 1, c);
+  int tmp = p1.toInt(),
+    mot = ((tmp+2) % 2) + 1,
+    addr = ADDR [(tmp - 1)/2],
+    pow = p2.toInt() - 128;
+  Serial.println ("p1: " + p1 + "  addr: " + String(addr) + "  pow: " + String(pow) + "  mot: " + String(mot));
+  
+  if (pow == 0) {
+      Serial.println ("Halt");
+    halt (addr);
+  } else if (pow > 0) {
+      Serial.println ("F");
+    fwd (addr, pow, mot);
+  } else {
+      Serial.println ("B");
+    bwd (addr, 0 - pow, mot);
   }
-}
-void SabertoothMotor::bwd(int data, int n) {
-  switch (n) {
-    case 0: // Both motors
-      run (MOTOR1_BWD, data);
-      run (MOTOR2_BWD, data);
-      break;
-    case 1: // First motor
-      run (MOTOR1_BWD, data);
-      break;
-    case 2:  // Second motor
-      run (MOTOR2_BWD, data);
-      break;
-    default: break;
-  }
-}
-void SabertoothMotor::lht(int data) {  // Turns from still position
-  run (MOTOR1_FWD, data);
-  run (MOTOR2_BWD, data);
-}
-void SabertoothMotor::rht(int data) {  // Turns from still position
-  run (MOTOR2_FWD, data);
-  run (MOTOR1_BWD, data);
-}
-
-void SabertoothMotor::fmt(int m1, int m2) {  // Forward moving turn
-  run (MOTOR1_FWD, m1);
-  run (MOTOR2_FWD, m2);
-}
-
-void SabertoothMotor::bmt(int m1, int m2) {  //  Backward moving turn
-  run (MOTOR1_BWD, m1);
-  run (MOTOR2_BWD, m2);
-}
-
-/* *************************************************************************** */
-
-/* 
-
-Same as the functions above, but take percentage values (in decimal form) instead of ints
-EX: fwd(0.2, 0)
-   -Runs both motors at 20% power (maybe)
-   -Only run when sent 'r' is a double
-
-*/
-
-void SabertoothMotor::fwd(double r, int n) {  // n = 0 by default
-  switch (n) {
-    case 0:  // Both motors
-      run (MOTOR1_FWD, r);
-      run (MOTOR2_FWD, r);
-      break;
-    case 1:  // First motor
-      run (MOTOR1_FWD, r);
-      break;
-    case 2:  // Second motor
-      run (MOTOR2_FWD, r);
-      break;
-    default: break;
-  }
-}
-void SabertoothMotor::bwd(double r, int n) {switch (n) {  // n = 0 by default
-    case 0:  // Both motors
-      run (MOTOR1_BWD, r);
-      run (MOTOR2_BWD, r);
-      break;
-    case 1:  // First motor
-      run (MOTOR1_BWD, r);
-      break;
-    case 2:  // Second motor
-      run (MOTOR2_BWD, r);
-      break;
-    default: break;
-  }
-}
-void SabertoothMotor::lht(double r) {
-  run (MOTOR1_FWD, r);
-  run (MOTOR2_BWD, r);
-}
-void SabertoothMotor::rht(double r) {
-  run (MOTOR1_BWD, r);
-  run (MOTOR2_FWD, r);
-}
-
-void SabertoothMotor::fmt(double m1, double m2) {  // Forwards moving turn
-  run (MOTOR1_FWD, m1);
-  run (MOTOR2_FWD, m2);
-}
-
-void SabertoothMotor::bmt(double m1, double m2) {  // Backwards moving turn
-  run (MOTOR1_BWD, m1);
-  run (MOTOR2_BWD, m2);
-}
-
-/* *************************************************************************** */
-
-void SabertoothMotor::stop () {
-  run (0, 0);  // Stops motor one
-  run (4, 0);  // Stops motor two
 }
--- Sabertooth/SabertoothMotor.h
+++ Sabertooth/SabertoothMotor.h
@@ -3,52 +3,25 @@
 
 #include "Arduino.h"
 #include "Stream.h"
-
-static const int MOTOR1_FWD = 0, //check documentation
-  MOTOR1_BWD = 1,
-  //MIN_V = 2,
-  //MAX_V = 3,
-  MOTOR2_FWD = 4,
-  MOTOR2_BWD = 5,
-  //MOTOR1_7BIT= 6,
-  //MOTOR2_7BIT= 7,
-  //FWD_MIX = 8,
-  //BWD_MIX = 9,
-  //RHT_MIX = 10,
-  //LHT_MIX = 11,
-  //FWD_7BIT = 12,
-  //TURN_7BIT = 13,
-  //STIM = 14,
-  //BAUD = 15,
-  RAMP = 16,
-  DBND = 17;
-      
+	
+static const int MOTOR1_BWD = 0, //check documentation
+	MOTOR1_FWD = 1,
+	MOTOR2_BWD = 4,
+	MOTOR2_FWD = 5,
+	RAMP = 16,
+	DBND = 17;
+		
 class SabertoothMotor { //i'm lazy so all these functions have 3-4 letter names
-  public:
-    SabertoothMotor ();
-    void init (int serial_idx, int addr, int max_vel = 127);
-
-    void run (int cmd, int data);
-    void fwd (int data, int n = 0); //n -> 0=both motors, 1=first motor, 2=second motor
-    void bwd (int data, int n = 0);
-    void lht (int data); //turn on the spot
-    void rht (int data);
-    void fmt (int m1, int m2); //forwards moving turn
-    void bmt (int m1, int m2); //backwards moving turn
-
-    void run (int cmd, double r);
-    void fwd (double r, int n = 0);
-    void bwd (double r, int n = 0);
-    void lht (double r);
-    void rht (double r);
-    void fmt (double m1, double m2); //forwards moving turn
-    void bmt (double m1, double m2); //backwards moving turn
-
-    void stop ();
-    
-  private:
-    int SERIAL_IDX;
-    int ADDR;
-    int MAX_VEL;
+	public:
+	SabertoothMotor ();
+	void init (int addr []);
+	void run (int addr, int cmd, int data);
+	void fwd (int addr, int data, int n = 0); //n -> 0=both motors, 1=first motor, 2=second motor
+	void bwd (int addr, int data, int n = 0);
+ 
+	void halt (int addr);
+	void read (String msg);
+	
+	int ADDR [5];
 };
 #endif
